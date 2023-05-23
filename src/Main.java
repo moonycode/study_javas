dbf6cc65e3a17a230d42c93952796d65cfbfa1a8
moonycode <chl93m@naver.com>
2023-05-23 13:15:41 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    String name="JeongMoon";
+    System.out.println(name);
+    // return 0;
+  }
+
+}
---
+++ src/Prints.java
@@ -0,0 +1,8 @@
+public class Prints {
+  public static void main(String[] args) 
+  
+  throws Exception {
+    String mystr="빅데이터 java 교육 과정";
+    System.out.println(mystr);
+}
+}
