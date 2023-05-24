f148d5b128d4d0b1a3c63126dbf67b33342165cb
moonycode <chl93m@naver.com>
2023-05-24 13:11:35 +0900

    baekjoon_2 correct

--- src/Baekjoon_2.java
+++ src/Baekjoon_2.java
@@ -6,18 +6,9 @@ public class Baekjoon_2 {
     // 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
     
     Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter A");
-
-    Scanner myObj2 = new Scanner(System.in);
-    System.out.println("Enter B");
-
-    int num1 = myObj.nextInt();
-    int num2 = myObj2.nextInt();
-    System.out.println(num1+"+"+num2+"="+ (num1+num2));
-
-
-
-
+    int A = myObj.nextInt();
+    int B = myObj2.nextInt();
+    System.out.println(A+B);
     // return 0;
   }
 }
