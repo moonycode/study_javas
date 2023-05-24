8d31badd845729157d44550fb637608d0e3be941
moonycode <chl93m@naver.com>
2023-05-24 16:12:28 +0900

    loopnestedloops

--- src/Scanners.java
+++ src/Scanners.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-public class Scanners {
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter username");
+// import java.util.Scanner;
+// public class Scanners {
+//   public static void main(String[] args) {
+//     Scanner myObj = new Scanner(System.in);
+//     System.out.println("Enter username");
 
-    String userName = myObj.nextLine();
-    System.out.println("Username is: "+userName);
+//     String userName = myObj.nextLine();
+//     System.out.println("Username is: "+userName);
 
-    // return 0;
-  }
-}
+//     // return 0;
+//   }
+// }
