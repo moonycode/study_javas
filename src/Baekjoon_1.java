94a1b982ff8548890219c3fd6ff53952c5c51020
moonycode <chl93m@naver.com>
2023-05-24 14:21:05 +0900

    baekjoon removed

--- src/Baekjoon_1.java
+++
@@ -1,8 +0,0 @@
-public class Baekjoon_1 {
-  public static void main(String[] args) {
-    // 참조 : https://www.acmicpc.net/problem/2557
-    // 문제 : Hello World!를 출력하시오.
-    System.out.println("Hello World!");
-    // return 0;
-  }
-}
