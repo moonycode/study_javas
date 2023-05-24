b1a453e0bf8259fbddacf1bb935f7a8614eaafd7
moonycode <chl93m@naver.com>
2023-05-24 15:23:10 +0900

    learning for문

---
+++ src/LoopFors.java
@@ -0,0 +1,14 @@
+public class LoopFors {
+  public static void main(String[] args) {
+  //for (statement 1; statement 2; statement 3){
+  //     code block to be executed
+  // }
+  int second = 10;
+
+  for (int first = 5; first<second; first = first + 2){
+    System.out.println(first);
+    }
+    System.out.println("End");
+    // return 0;
+  }
+}
---
+++ src/LoopWithMark.java
@@ -0,0 +1,11 @@
+public class LoopWithMark {
+  public static void main(String[] args) {
+    int usercount = 5;
+
+  for (int countline = 1; countline<usercount; countline = countline + 1){
+    System.out.println("for "+countline);
+    }
+    System.out.println("End");
+    // return 0;
+  }
+}
