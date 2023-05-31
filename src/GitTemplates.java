5d8ad6b4b2b6a6cbff7402c85891571c6dc98565
moonycode <chl93m@naver.com>
2023-05-31 11:04:25 +0900

    test changes

---
+++ src/GitTemplates.java
@@ -0,0 +1,10 @@
+public class GitTemplates{
+  public static void main(String[] args) {
+    try {
+      
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+    // return 0;
+  }
+}
