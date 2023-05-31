73bbe21c0911e8665b932b92962895cd608e13af
moonycode <chl93m@naver.com>
2023-05-31 11:55:25 +0900

    edit error

--- src/GitConflictsCopy.java
+++ src/GitConflictsCopy.java
@@ -1,15 +1,21 @@
 public class GitConflictsCopy {
   public static void main(String[] args) {
     try {
-<<<<<<< HEAD
+//       <<<<<<< HEAD
+//       int first = 2;
+//       first = first +1;
+// =======
+//       int second = 1;
+//       int first = 0;
+//       second = first + 1;
+// >>>>>>> faf4f26b4e5f72425e20c4819e44dee7949fa1ce
+
       int first = 2;
       first = first +1;
-=======
       int second = 1;
-      int first = 0;
+      first = 0;
       second = first + 1;
->>>>>>> faf4f26b4e5f72425e20c4819e44dee7949fa1ce
-    
+
     } catch (Exception e) {
       // TODO: handle exception
     }
