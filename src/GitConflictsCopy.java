ee75d765e1e4f3ff35f57cb1b67c1361c2b66594
moonycode <chl93m@naver.com>
2023-05-31 11:50:18 +0900

    GitConflictsCopy

---
+++ src/GitConflictsCopy.java
@@ -0,0 +1,13 @@
+public class GitConflictsCopy {
+  public static void main(String[] args) {
+    try {
+      int second = 1;
+      int first = 0;
+      second = first + 1;
+    
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+    // return 0;
+}
+}
