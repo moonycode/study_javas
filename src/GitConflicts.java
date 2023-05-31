2bc8b6323bc06ec9e00dcc07c83ac577a2c29521
moonycode <chl93m@naver.com>
2023-05-31 11:46:16 +0900

    Git Conflict Mine

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,8 +1,9 @@
 public class GitConflicts {
   public static void main(String[] args) {
     try {
-      int first = 2;
-      first = first + 1;
+      int second = 1;
+      int first = 0;
+      second = first + 1;
     
     } catch (Exception e) {
       // TODO: handle exception
