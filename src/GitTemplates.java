1508746636633ac5654261a9d088f7ef9f804346
moonycode <chl93m@naver.com>
2023-05-31 11:09:24 +0900

    git template second

--- src/GitTemplates.java
+++ src/GitTemplates.java
@@ -1,7 +1,9 @@
 public class GitTemplates{
   public static void main(String[] args) {
     try {
-      
+      int first = 0;
+      int second = 1;
+      second = first + 1;
     } catch (Exception e) {
       // TODO: handle exception
     }
