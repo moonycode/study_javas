10b3a83d333232fcfdf72199b0d125400199ec35
moonycode <chl93m@naver.com>
2023-05-31 11:43:44 +0900

    Second GitConflicts

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,9 +1,8 @@
 public class GitConflicts {
   public static void main(String[] args) {
     try {
-      int second = 1;
-      int first = 0;
-      second = first + 1;
+      int first = 2;
+      first = first + 1;
     
     } catch (Exception e) {
       // TODO: handle exception
