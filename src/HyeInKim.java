9c3404a3fd0890ede62c2d7445138b28836f9c3b
aabchyein <abcqaz369@naver.com>
2023-05-31 12:17:38 +0900

    파일 포함

---
+++ src/HyeInKim.java
@@ -0,0 +1,14 @@
+/**
+ * HyeInKim
+ */
+public class HyeInKim {
+
+    public static void main(String[] args) {
+        try {
+           System.out.println("김혜인"); 
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+}
