222c60570cf31fe5c485ea9bad3ef334730ad84b
moonycode <chl93m@naver.com>
2023-05-31 12:17:36 +0900

    Invited jeongmoon

---
+++ src/JeongMoonChoi.java
@@ -0,0 +1,10 @@
+public class JeongMoonChoi {
+public static void main(String[] args) {
+  try {
+    System.out.println("최정문");
+  } catch (Exception e) {
+    // TODO: handle exception
+  }
+  // return 0;
+}   
+}
