08b45dcd272560530fc5423afcf0dbbe2f58d5c1
moonycode <chl93m@naver.com>
2023-05-31 11:09:04 +0900

    gittemplates first

--- src/GitTemplates.java
+++ src/GitTemplates.java
@@ -1,7 +1,8 @@
 public class GitTemplates{
   public static void main(String[] args) {
     try {
-      
+      int first = 0;
+      first = first + 1;
     } catch (Exception e) {
       // TODO: handle exception
     }
